512cc75765871f8f3ea049b1e9e03e605ccbdfef
JEPTAC <j.perez@ei.com.co>
2026-07-09 20:26:44 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-var CACHE_VERSION = "ei-trazabilidad-v203-recepcion-pedidos-dos-botones-pdf-csv";
+var CACHE_VERSION = "ei-trazabilidad-v204-fix-drawer-recepcion-pedidos";
 self.addEventListener("install",function(event){
   self.skipWaiting();
   event.waitUntil(caches.keys().then(function(keys){
